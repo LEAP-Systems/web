830635b910832145a2b4092d63c2fe388f4d92dd
Brandon Lucas Zhu <Brandonlucaszhu@gmail.com>
2020-08-06 15:44:15 -0400

    Set-up of mission section of webpage

---
+++ leap-web/src/components/missionContent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MissionContent extends Component {
+    state = {  }
+    render() { 
+        return (  
+            
+
+        );
+    }
+}
+ 
+export default MissionContent;
--- leap-web/src/components/parallaxWelcomeImage.js
+++ leap-web/src/components/parallaxWelcomeImage.js
@@ -3,15 +3,16 @@ import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 import welcomeImage from "../images/leap_proj2.jpg";
 import Image from "react-bootstrap/Image";
 import { Container } from "react-bootstrap";
+import "../css/mainpage.css";
 
 class ParallaxWelcomeImage extends Component {
   state = {};
   render() {
     return (
-      <Container fluid>
+      <Container fluid className="max-width">
         <ParallaxProvider>
           <Parallax z={[-20, 20]} tagOuter="figure">
-            <Image src={welcomeImage} fluid />
+            <Image className="para-img" src={welcomeImage} />
           </Parallax>
         </ParallaxProvider>
       </Container>
