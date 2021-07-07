536bc2e2d69d01e81e6b46f87f6a703596e8989d
cSDes1gn <christian911@sympatico.ca>
2021-07-06 23:12:09 -0400

    add first info section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import Info from '../components/Info';
+import { mission } from '../components/Info/data';
 
 const Home = () => {
 	const [open, setOpen] = useState(false);
@@ -14,6 +16,7 @@ const Home = () => {
       <Navbar open={open} toggle={toggle}/>
       <Sidebar open={open} toggle={toggle}/>
       <Hero/>
+      <Info {...mission}/>
 		</>
 	);
 }
