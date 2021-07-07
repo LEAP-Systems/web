fa49adfd070e80a9617ff3d63dad645a14b4de6b
cSDes1gn <christian911@sympatico.ca>
2021-07-06 23:11:35 -0400

    create data file for mission page

---
+++ src/components/Info/data.js
@@ -0,0 +1,17 @@
+export const mission = {
+	id: 'mission',
+	light: false,
+  light_text: true,
+  light_text_desc: true,
+  topline: 'Mission',
+  headline: 'Securing LiFi',
+  img_start: false,
+  img: require('../../images/mission.svg').default,
+  alt: 'Christian',
+  dark: true,
+  primary: true,
+  dark_text: false,
+  description: "Light Emitting Access Point (LEAP) is a concept for short \
+  range transmission solution for sensitive networks. LEAP exploits directional multicast \
+  to increase security while decoupling positional information for each connected device.",
+}
