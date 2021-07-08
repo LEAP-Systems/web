11c379534cf575731a56ca6239f7d1b0f9a48a45
cSDes1gn <christian911@sympatico.ca>
2021-07-08 00:21:54 -0400

    remove old webfont loader

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/mainpage.css";
-// import "react-slideshow-image/dist/styles.css";
 import "react-multi-carousel/lib/styles.css";
-
-import WebFont from 'webfontloader';
 import App from "./App";
 
 ReactDOM.render(
@@ -17,17 +11,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-WebFont.load({
-  google: {
-    families: ['Josefin Sans:300,400,700', 'sans-serif']
-  }
-});
-WebFont.load({
-  google: {
-    families: ['Libre Barcode 39 Text:300,400,700', 'cursive']
-  }
-});
-  
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
