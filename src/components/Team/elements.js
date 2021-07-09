213fdb0a11d9bd6f5b9d7a7ee4e1727f011c9534
cSDes1gn <christian911@sympatico.ca>
2021-07-09 00:54:42 -0400

    team component wrapper for member cards

---
+++ src/components/Team/elements.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const TeamContainer = styled.div`
+  height: 800px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #0c0c0c;
+  @media screen and (max-width: 768px) {
+    height: 1100px;
+  }
+  @media screen and (max-width: 480px) {
+    height: 1300px;
+  }
+`
+
+export const TeamWrapper = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+  grid-gap: 16px;
+  padding: 0 50px;
+  @media screen and (max-width: 1000px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+    padding: 0 20px;
+  }
+`
+
+export const TeamH1 = styled.h1`
+  font-size: 2.5rem;
+  color: #fff;
+  margin-bottom: 64px;
+  @media screen and (max-width: 480px) {
+    font-size: 2rem;
+  }
+`
