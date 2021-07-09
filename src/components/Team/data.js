355bdb366ed6324275bd12489e9ba0469489c491
cSDes1gn <christian911@sympatico.ca>
2021-07-09 00:54:25 -0400

    add data file for team cards

---
+++ src/components/Team/data.js
@@ -0,0 +1,27 @@
+export const christian = {
+  src: require('../../images/christian.png').default,
+  alt: 'christian',
+  header: 'Christian Sargusingh - B.Eng Computer Systems',
+  description: 'Lead systems engineer and team coordinator developing system hardware, firmware and embedded IoT components.',
+}
+
+export const steven = {
+  src: require('../../images/steven.png').default,
+  alt: 'steven',
+  header: 'Steven Zhou - B.Eng Computer Systems',
+  description: 'Systems, software and QA engineer responsible for and implementing and quality control of system components.',
+}
+
+export const brandon = {
+  src: require('../../images/brandon.png').default,
+  alt: 'brandon',
+  header: 'Brandon Zhu - B.Eng Electrical',
+  description: 'Webfront and systems developer heading the Computer Vision project for efficient 3D spatial decoding.',
+}
+
+export const aaron = {
+  src: require('../../images/aaron.png').default,
+  alt: 'aaron',
+  header: 'Aaron Huang - B.Eng Electrical',
+  description: 'Hardware engineer and designer of the original 64-bit LEAP transmitter prototype.',
+}
