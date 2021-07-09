6d1b32bb8bc8fe257cddb0afa0e985b2abfe6bff
cSDes1gn <christian911@sympatico.ca>
2021-07-09 00:54:59 -0400

    add team component to pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,29 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Info from '../components/Info';
-import { accolades, design, mission, recruit, team} from '../components/Info/data';
+import Team from '../components/Team';
+import { accolades, design, mission, recruit } from '../components/Info/data';
 
 const Home = () => {
-	const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
   const toggle = () => {
     setOpen(!open)
   };
 
   return (
-		<>
+    <>
       <Navbar open={open} toggle={toggle}/>
       <Sidebar open={open} toggle={toggle}/>
       <Hero/>
       <Info {...mission}/>
       <Info {...design}/>
-      <Info {...team}/>
+      <Team id='team'/>
       <Info {...recruit}/>
       <Info {...accolades}/>
-		</>
-	);
+    </>
+  );
 }
 
 export default Home;
