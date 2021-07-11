ecab1d9854fb7a25b4c7da34bbe4d5319521ad86
cSDes1gn <christian911@sympatico.ca>
2021-07-11 10:46:25 -0400

    implement home route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-// import logo from './images/LEAP_BANNER_INV_NT.png';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages';
-// import TopBar from "./components/topBar";
-// import Home from "./components/home";
-// import MissionContent from "./components/missionContent";
-// import AccompContent from "./components/accompContent";
-// import AboutContent from "./components/aboutContent";
-// import TeamContent from "./components/teamContent";
-// import FooterMenu from "./components/footerMenu";
-// import RecruitContent from "./components/recruitContent";
 
 function App() {
   return (
     <Router>
-     <Home /> 
+      <Switch>
+        <Route path='/' component={Home} exact></Route>
+      </Switch>
     </Router>
   );
 }
