78d99936323706723dbc8fe7232fd6aef34a4883
cSDes1gn <christian911@sympatico.ca>
2021-07-11 10:46:34 -0400

    update card styling

--- src/components/Card/elements.js
+++ src/components/Card/elements.js
@@ -13,8 +13,9 @@ export const CardContainer = styled.div`
   transition: all 0.2s ease-in-out;
   &:hover {
     transform: scale(1.02);
+    background: #000;
+    color: #fff;
     transition: all 0.2s ease-in-out;
-    cursor: pointer;
   }
 `
 
@@ -26,10 +27,37 @@ export const CardIcon = styled.img`
 
 export const CardH2 = styled.h2`
   font-size: 1rem;
+  color: #855dd4;
   margin-bottom: 10px;
+  color: '#fff';
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: 700;
+  letter-spacing: 1.4px;
+  text-transform: uppercase;
+  margin-bottom: 16px;
 `
 
 export const CardP = styled.p`
   font-size: 1rem;
   text-align: left;
+`
+
+export const CardSocialsWrapper = styled.div`
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  grid-gap: 16px;
+  padding: 0 50px;
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+    padding: 0 20px;
+  }
+`
+
+export const CardSocial = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-bottom: 10px;
 `
