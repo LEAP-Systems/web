d1f7188e2fbacd13ccd92e461193b2ec56f3ab93
cSDes1gn <christian911@sympatico.ca>
2021-07-11 10:47:24 -0400

    todo: card socials

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { CardContainer, CardH2, CardIcon, CardP } from './elements'
 
-const Card = ({src, alt, header, description}) => {
+import { CardContainer, CardH2, CardIcon, CardP, CardSocial, CardSocialsWrapper} from './elements'
+
+const Card = ({src, alt, header, description, github, linkedin}) => {
   return (
     <>
       <CardContainer>
         <CardIcon src={src} alt={alt}/>
         <CardH2>{header}</CardH2>
         <CardP>{description}</CardP>
+        {/* <CardSocialsWrapper>
+          <CardSocial src={require('../../images/github.png').default} href={github}></CardSocial>
+          <CardSocial src={require('../../images/linkedin.png').default} href={linkedin}></CardSocial> */}
+        {/* </CardSocialsWrapper> */}
       </CardContainer>
     </>
   )
