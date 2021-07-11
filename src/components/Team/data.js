24b1bbe250ed91d5fd39fbc347d6df53cb5df83c
cSDes1gn <christian911@sympatico.ca>
2021-07-11 11:33:32 -0400

    update team data model

--- src/components/Team/data.js
+++ src/components/Team/data.js
@@ -1,27 +1,35 @@
 export const christian = {
   src: require('../../images/christian.png').default,
   alt: 'christian',
-  header: 'Christian Sargusingh - B.Eng Computer Systems',
-  description: 'Lead systems engineer and team coordinator developing system hardware, firmware and embedded IoT components.',
+  header: 'Christian Sargusingh',
+  description: 'Team lead & systems',
+  linkedin: 'https://www.linkedin.com/in/christian-sargusingh/',
+  github: 'https://github.com/cSDes1gn'
 }
 
 export const steven = {
   src: require('../../images/steven.png').default,
   alt: 'steven',
-  header: 'Steven Zhou - B.Eng Computer Systems',
-  description: 'Systems, software and QA engineer responsible for and implementing and quality control of system components.',
+  header: 'Steven Zhou',
+  description: 'Systems & software',
+  linkedin: 'https://www.linkedin.com/in/christian-sargusingh/',
+  github: 'https://github.com/cSDes1gn'
 }
 
 export const brandon = {
   src: require('../../images/brandon.png').default,
   alt: 'brandon',
-  header: 'Brandon Zhu - B.Eng Electrical',
-  description: 'Webfront and systems developer heading the Computer Vision project for efficient 3D spatial decoding.',
+  header: 'Brandon Zhu',
+  description: 'Computer Vision & machine learning',
+  linkedin: 'https://www.linkedin.com/in/christian-sargusingh/',
+  github: 'https://github.com/cSDes1gn'
 }
 
 export const aaron = {
   src: require('../../images/aaron.png').default,
   alt: 'aaron',
-  header: 'Aaron Huang - B.Eng Electrical',
-  description: 'Hardware engineer and designer of the original 64-bit LEAP transmitter prototype.',
+  header: 'Aaron Huang',
+  description: 'Hardware & Computer Vision engineer',
+  linkedin: 'https://www.linkedin.com/in/christian-sargusingh/',
+  github: 'https://github.com/cSDes1gn'
 }
