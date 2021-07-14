f2c00b8f9534d039f955438f2f742e0f54506715
cSDes1gn <christian911@sympatico.ca>
2021-07-14 02:22:14 -0400

    design footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { FaYoutube, FaGithub } from "react-icons/fa";
+import {
+  FooterContainer,
+  FooterWrapper,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  External,
+  ExternalWrapper,
+  Icons,
+  IconLink,
+  WebsiteRights,
+  Home,
+  Logo,
+} from "./elements";
+import logo from "../../images/LEAP_INS_WHITE.png";
+
+const Footer = () => {
+  return (
+    <>
+      <FooterContainer>
+        <FooterWrapper>
+          <FooterLinksContainer>
+            <FooterLinksWrapper>
+              <FooterLinkItems>
+                <FooterLinkTitle>Services</FooterLinkTitle>
+                <FooterLink href="https://github.com/LEAP-Systems/web/issues/new">
+                  Report Issue
+                </FooterLink>
+                <FooterLink href="mailto:christian@leapsystems.online">
+                  Contact Us
+                </FooterLink>
+                <FooterLink href={"/"}>Terms of Service</FooterLink>
+              </FooterLinkItems>
+              <FooterLinkItems>
+                <FooterLinkTitle>Media</FooterLinkTitle>
+                <FooterLink href={"https://youtu.be/aiTprGXODSQ"}>
+                  Promotional Video
+                </FooterLink>
+                <FooterLink
+                  href={
+                    "https://carleton.ca/sce/2020/systems-capstone-project-won-the-w-e-cowie-innovation-award/"
+                  }
+                >
+                  Cowie Innovation Award
+                </FooterLink>
+              </FooterLinkItems>
+            </FooterLinksWrapper>
+          </FooterLinksContainer>
+          <External>
+            <ExternalWrapper>
+              <Home to="/">
+                <Logo src={logo} alt="LEAP" width="80" height="80"></Logo>
+              </Home>
+              <WebsiteRights>
+                LEAP Systems © {new Date().getFullYear()}. All Rights Reserved
+              </WebsiteRights>
+              <Icons>
+                <IconLink
+                  href="https://github.com/LEAP-Systems"
+                  target="_blank"
+                  aria-label="Github"
+                >
+                  <FaGithub />
+                </IconLink>
+                <IconLink
+                  href="https://youtu.be/aiTprGXODSQ"
+                  target="_blank"
+                  aria-label="YouTube"
+                >
+                  <FaYoutube />
+                </IconLink>
+              </Icons>
+            </ExternalWrapper>
+          </External>
+        </FooterWrapper>
+      </FooterContainer>
+    </>
+  );
+};
+
+export default Footer;
