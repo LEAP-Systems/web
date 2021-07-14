e82e5201ad655fcdef4b2cf3975b4b9469132977
cSDes1gn <christian911@sympatico.ca>
2021-07-14 02:22:05 -0400

    add footer elements

---
+++ src/components/Footer/elements.js
@@ -0,0 +1,114 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const FooterContainer = styled.footer`
+  background-color: #140f21;
+`;
+
+export const FooterWrapper = styled.div`
+  padding: 48px 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1100px;
+  margin: 0 auto;
+`;
+
+export const FooterLinksContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  @media screen and (max-width: 820px) {
+    padding-top: 32px;
+  }
+`;
+
+export const FooterLinksWrapper = styled.div`
+  display: flex;
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`;
+
+export const FooterLinkItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 16px;
+  width: 160px;
+  box-sizing: border-box;
+  color: #fff;
+  @media screen and (max-width: 420px) {
+    margin: 0;
+    padding: 10px;
+    width: 100%;
+  }
+`;
+
+export const FooterLinkTitle = styled.h1`
+  font-size: 14px;
+  margin-bottom: 16px;
+`;
+
+export const FooterLink = styled.a`
+  color: #fff;
+  text-decoration: none;
+  margin-bottom: 0.5rem;
+  font-size: 14px;
+  &:hover {
+    color: #855dd4;
+    transition: 0.2s ease-out;
+  }
+`;
+
+export const External = styled.section`
+  max-width: 1000px;
+  width: 100%;
+  padding: 0 60px;
+`;
+
+export const ExternalWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1100px;
+  margin: 40px auto 0 auto;
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`;
+
+export const Home = styled(Link)`
+  color: #fff;
+  cursor: pointer;
+  align-items: center;
+`;
+
+export const Logo = styled.img`
+  color: #fff;
+  justify-self: start;
+  cursor: pointer;
+  align-items: center;
+  margin-bottom: 16px;
+  display: flex;
+  font-size: 1.5rem;
+  text-decoration: none;
+`;
+
+export const WebsiteRights = styled.small`
+  color: #fff;
+  margin-bottom: 16px;
+  text-align: center;
+`;
+
+export const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100px;
+`;
+
+export const IconLink = styled.a`
+  color: #fff;
+  font-size: 24px;
+`;
