83cdf40e520397a703699fe0ff92e3630943914d
cSDes1gn <christian911@sympatico.ca>
2021-07-14 21:09:41 -0400

    implement react smoothscroll on nav clicks

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
+import { animateScroll as scroll } from "react-scroll";
 import {
   NavLogo,
   Nav,
@@ -12,11 +13,29 @@ import {
 import logo from "../../images/LEAP_INS_WHITE.png";
 
 const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
+
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+
   return (
     <>
-      <Nav>
+      <Nav scrollNav={scrollNav}>
         <NavbarContainer>
-          <NavLogo to="/">
+          <NavLogo to="/" onClick={toggleHome}>
             <img src={logo} alt="LEAP" width="60" height="60"></img>
           </NavLogo>
           <HamIcon onClick={toggle}>
@@ -24,19 +43,64 @@ const Navbar = ({ toggle }) => {
           </HamIcon>
           <NavMenu>
             <NavItem>
-              <NavLinks to="mission">Mission</NavLinks>
+              <NavLinks
+                to="mission"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                Mission
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="design">Design</NavLinks>
+              <NavLinks
+                to="design"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                Design
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="accolades">Accolades</NavLinks>
+              <NavLinks
+                to="accolades"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                Accolades
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="team">Team</NavLinks>
+              <NavLinks
+                to="team"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                Team
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="recruit">Join</NavLinks>
+              <NavLinks
+                to="recruit"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                Join
+              </NavLinks>
             </NavItem>
           </NavMenu>
         </NavbarContainer>
