5fc64b79e7b12b4676ed5c29858bdbe19123d76c
ztnel <christian911@sympatico.ca>
2022-10-10 22:32:44 -0400

    react router dom api updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" component={Home} exact></Route>
-      </Switch>
+      <Routes>
+        <Route path="/" exact element={<Home />}></Route>
+      </Routes>
     </Router>
   );
 }
