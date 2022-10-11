bd160b33c13a8020d117d0c2f2cf6bf36de64f7c
ztnel <christian911@sympatico.ca>
2022-10-11 00:57:21 -0400

    add aos to team container

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "../Card";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { aaron, brandon, christian, steven } from "./data";
 import { TeamContainer, TeamWrapper, TeamH1, TopLine } from "./elements";
 
 const Team = ({ id }) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <>
       <TeamContainer id={id}>
-        <TopLine>Our Team</TopLine>
-        <TeamH1>Follow Us</TeamH1>
+        <TopLine data-aos-duration="1000" data-aos="fade-left">
+          Our Team
+        </TopLine>
+        <TeamH1
+          data-aos="fade-left"
+          data-aos-duration="1000"
+          data-aos-delay="100"
+        >
+          Follow Us
+        </TeamH1>
         <TeamWrapper>
           <Card {...christian} />
           <Card {...brandon} />
